86b2ee394e66987511e2b9435367389a16a905c3
Benjamin Roos <BenRRoos@gmail.com>
2022-10-20 22:03:48 -0500

    package cleanup

--- db/config.py
+++
@@ -1,8 +0,0 @@
-import os
-import django
-
-def configureDjango():
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'django_settings'
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_settings")
-    django.setup()
-
